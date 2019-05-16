5c948e1a452fb06df108e55936ab72a10ab0ed7e
chenshuaijun <shuaijunc@globalcrown.com.cn>
2019-05-16 19:29:59 +0800

    a

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/***********************************************
+
+    Filename: main.cpp
+    Author: 
+    Description:
+    Create Data:    2019-05-16 19:03:50
+    Modfiy History: 2019-05-16 19:03:50
+
+***********************************************/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    string hello = "hello world";
+
+    cout << hello << endl;
+}
